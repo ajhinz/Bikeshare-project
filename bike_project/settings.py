94c6301b69851b337efd4de8a7b89c1494636432
Chris Anderson <christopher.anderson@gmail.com>
2011-04-13 01:43:24 -0400

    Added CSS framework

--- bike_project/settings.py
+++ bike_project/settings.py
@@ -17,13 +17,13 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'bike_project',                      # Or path to database file if using sqlite3.
-        'USER': 'adhinz',                      # Not used with sqlite3.
-        'PASSWORD': 'adhinzpass',              # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+    'NAME': 'testdb',                      # Or path to database file if using sqlite3.
+    'USER': 'chris',                      # Not used with sqlite3.
+    'PASSWORD': '3.14159',              # Not used with sqlite3.
+    'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+    'PORT': '',
     }
 }
 
