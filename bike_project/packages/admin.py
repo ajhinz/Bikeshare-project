d8a89a2a5375b4a8a47c020ddd9a5f5169ef6023
Adam Hinz <adam.hinz@gmail.com>
2011-04-15 19:29:23 -0400

    Make RouteLocation available from the admin interface

--- bike_project/packages/admin.py
+++ bike_project/packages/admin.py
@@ -1,6 +1,7 @@
-from packages.models import Route, BikeshareUpdate, BikeshareStation
+from packages.models import Route, RouteLocation, BikeshareUpdate, BikeshareStation
 from django.contrib import admin
 
 admin.site.register(Route)
+admin.site.register(RouteLocation)
 admin.site.register(BikeshareUpdate)
 admin.site.register(BikeshareStation)
